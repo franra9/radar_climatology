3ddec6a085baadbdcf985f3231e4a4645deb6652
franra9 <francescrouradserias@gmail.com>
2020-08-08 16:27:51 +0200

    reorganize anual sum

--- R/TFG_anual.R
+++ R/TFG_anual.R
@@ -1,24 +1,29 @@
 # Francesc Roura Adserias # treball final de grau
 # this script sums up monthly data to perform anual accumulation.
 
-setwd("/home/francesc/data/")
+source("/home/francesc/repositories/radar_climatology/R/TFG.config.R")
 
-#library(tiff)
-#library(sp)
-#library(raster)
-#library(rgdal)
+year.ini <- format(as.Date(as.character(2015), format="%Y"),"%Y")
+year.fin <- format(as.Date(as.character(2016), format="%Y"),"%Y")
+
+filei <- "radar_SMC_ppt_TFG/CMP24KG_24h/monthly_agg_corr/monthly_agg_XXX_RNN_"
+fileo <- "~/results/radar_SMC_ppt_TFG/CMP24KG_24h/year_agg_corr/"
+
+dir.create(path=fileo, showWarnings=T, recursive=T)
+
+setwd("/home/francesc/data/")
 
 sum <- 0
 
 #### 24h aggregate ####
-for(year in 2015:2016){ # 7 anys, dels que volem trobar-ne la suma.
+for(year in year.ini:year.fin){ # 7 anys, dels que volem trobar-ne la suma.
   
   for(month in 1:12){ # recorrem tots els mesos de l'any
     
     if(month < 10){ # cosa del nomdel filein
-      filein  <- paste0("/radar_SMC_ppt_TFG/CMP24KG_24h/monthly_agg_corr/monthly_agg_XXX_RNN_",as.character(year),"_0",as.character(month),"_CMP24KG_24h_A.tif")
+      filein  <- paste0(filei, as.character(year),"_0",as.character(month),"_CMP24KG_24h_A.tif")
     }else{
-      filein  <- paste0("/radar_SMC_ppt_TFG/CMP24KG_24h/monthly_agg_corr/monthly_agg_XXX_RNN_",as.character(year),"_",as.character(month),"_CMP24KG_24h_A.tif")
+      filein  <- paste0(filei, as.character(year),"_",as.character(month),"_CMP24KG_24h_A.tif")
     }
     
     if(!file.exists(filein)){ # si el fitxer no existeix, no sumem res.
@@ -27,16 +32,17 @@ for(year in 2015:2016){ # 7 anys, dels que volem trobar-ne la suma.
       tif <- raster(filein)
       sum <- sum + tif
     }
+print(month)
 
   if(month == 12){ # escrivim la suma anual
     
-    fileout <- paste0("~/results/radar_SMC_ppt_TFG/CMP24KG_24h/year_agg_corr/year_agg_XXX_RNN_",
+    fileout <- paste0(fileo,
+		      "year_agg_XXX_RNN_",
                       as.character(year),
                       "_",
                       "CMP24KG_24h_A.tif")
     
     writeRaster(sum, fileout, overwrite=T)
-    
     sum <- 0
     }
   }
