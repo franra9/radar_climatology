4921551e4536a251b70fd102082ebb75a716d9b4
franra9 <francescrouradserias@gmail.com>
2021-04-14 14:26:10 +0200

    hourly workflow finalized! :)

--- R/config.R
+++ R/config.R
@@ -29,9 +29,9 @@ gpclibPermit()
 rep.dir <- "/home/francesc/repositories/radar_climatology/"
 data.dir <- "/home/francesc/data/radar_SMC_ppt_TFG/"
 outdir <- "/home/francesc/results/radar_climatology/"
-shp.dir <- "/home/francesc/data/radar_SMC_ppt_TFG/shape/LIASE_extremes.shp"
-shp.name <- "LIASE_A"
-int_an <- F #intensity analysis (expensive in computing default=F)
+shp.dir <- "/home/francesc/data/radar_SMC_ppt_TFG/shape/LIASE_area_interior.shp"
+shp.name <- "LIASE_a"
+int_an <- T #intensity analysis (expensive in computing default=F)
  
 print("Configuration has been loaded, with this parameters:")
 cat("Repository directory: "); print(rep.dir)
