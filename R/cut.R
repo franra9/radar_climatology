429d5dc2a0f8b6f3cd96619622f6da467e004fcc
franra9 <francescrouradserias@gmail.com>
2021-04-15 22:36:32 +0200

    add try statement because problems in some nov 2014 hourly files

--- R/cut.R
+++ R/cut.R
@@ -9,11 +9,14 @@ cut_files <- function(available.files = NULL, shp.dir = NULL, outdir = NULL){
 		shape <- shapefile(shp.dir)
 		files.list <- paste0(data.dir, available.files)
 		for(i in 1:length(files.list)){
-			tif <- raster(files.list[i])
-			tiff <- projectRaster(from = tif, crs = crs("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
-			area <- crop(tiff, shape) #crop image
-			dir.create(path = paste0(outdir, shp.name, available.files[i]), recursive=T)
-			writeRaster(area, paste0(outdir, shp.name, available.files[i]), overwrite=T) #save cut image
+		  if(i %% 100 == 0){
+		    print(available.files[i])
+		  }
+		  try(tif <- raster(files.list[i]))
+			try(tiff <- projectRaster(from = tif, crs = crs("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")))
+			try(area <- crop(tiff, shape)) #crop image)
+			  dir.create(path = paste0(outdir, shp.name, available.files[i]), recursive=T)
+			try(writeRaster(area, paste0(outdir, shp.name, available.files[i]), overwrite=T)) #save cut image
 		}
 		print(paste0("Existing", month, "files from ", date.ini," to ", date.fin, " have been cut and saved at", outdir, shp.name, "/"))
 	}
