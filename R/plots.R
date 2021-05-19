7661d9b7088c5ec3d1f904e678ba93376fc15265
franra9 <francescrouradserias@gmail.com>
2021-05-19 13:29:27 +0200

    minor

--- R/plots.R
+++ R/plots.R
@@ -25,8 +25,8 @@ months <- c("jan",
             "oct",
             "nov",
             "dec")
-apendix <- c(paste0(months[1:9],"_2013-2019_corr.tif"),
-             paste0(months[10:12],"_2013-2018_corr.tif"))
+apendix <- c(paste0(months[1:3],"_2013-2021_corr.tif"),
+             paste0(months[4:12],"_2013-2020_corr.tif"))
 
 #for(i in 1:12){
 #        tif <- raster(paste0(outdir,"/", shp.name,"/month/", apendix[i]))
@@ -47,7 +47,7 @@ apendix <- c(paste0(months[1:9],"_2013-2019_corr.tif"),
 #dev.off()
 #stop()
 
-tif <- raster(paste0(outdir,"/", shp.name,"/month/jan_2013-2019_corr.tif"))
+tif <- raster(paste0(outdir,"/", shp.name,"/month/jan_2013-2021_corr.tif"))
 png(paste0(outdir,"/plots/jan.png"))
 spplot(tif,
        main=list(label="Jan mean precipitation (mm)",cex=1),
@@ -61,7 +61,7 @@ spplot(tif,
 # Close the png file
 dev.off() 
 
-tif <- raster(paste0(outdir,"/", shp.name,"/month/feb_2013-2019_corr.tif"))
+tif <- raster(paste0(outdir,"/", shp.name,"/month/feb_2013-2021_corr.tif"))
 png(paste0(outdir,"/plots/feb.png"))
 spplot(tif,
        main=list(label="Feb mean precipitation (mm)",cex=1),
@@ -75,7 +75,7 @@ spplot(tif,
 # Close the png file
 dev.off() 
 
-tif <- raster(paste0(outdir,"/", shp.name,"/month/mar_2013-2019_corr.tif"))
+tif <- raster(paste0(outdir,"/", shp.name,"/month/mar_2013-2021_corr.tif"))
 png(paste0(outdir,"/plots/mar.png"))
 spplot(tif,
        main=list(label="Mar mean precipitation (mm)",cex=1),
@@ -89,7 +89,7 @@ spplot(tif,
 # Close the png file
 dev.off() 
 
-tif <- raster(paste0(outdir,"/", shp.name,"/month/apr_2013-2019_corr.tif"))
+tif <- raster(paste0(outdir,"/", shp.name,"/month/apr_2013-2020_corr.tif"))
 png(paste0(outdir,"/plots/apr.png"))
 spplot(tif,
        main=list(label="Apr mean precipitation (mm)",cex=1),
@@ -103,7 +103,7 @@ spplot(tif,
 # Close the png file
 dev.off() 
 
-tif <- raster(paste0(outdir,"/", shp.name,"/month/may_2013-2019_corr.tif"))
+tif <- raster(paste0(outdir,"/", shp.name,"/month/may_2013-2020_corr.tif"))
 png(paste0(outdir,"/plots/may.png"))
 spplot(tif,
        main=list(label="May mean precipitation (mm)",cex=1),
@@ -117,7 +117,7 @@ spplot(tif,
 # Close the png file
 dev.off() 
 
-tif <- raster(paste0(outdir,"/", shp.name,"/month/jun_2013-2019_corr.tif"))
+tif <- raster(paste0(outdir,"/", shp.name,"/month/jun_2013-2020_corr.tif"))
 png(paste0(outdir,"/plots/jun.png"))
 spplot(tif,
        main=list(label="Jun mean precipitation (mm)",cex=1),
@@ -131,7 +131,7 @@ spplot(tif,
 # Close the png file
 dev.off() 
 
-tif <- raster(paste0(outdir,"/", shp.name,"/month/jul_2013-2019_corr.tif"))
+tif <- raster(paste0(outdir,"/", shp.name,"/month/jul_2013-2020_corr.tif"))
 png(paste0(outdir,"/plots/jul.png"))
 spplot(tif,
        main=list(label="Jul mean precipitation (mm)",cex=1),
@@ -144,7 +144,7 @@ spplot(tif,
                         list(comarques, fill=NA, first=FALSE)))
 # Close the png file
 dev.off() 
-tif <- raster(paste0(outdir,"/", shp.name,"/month/aug_2013-2019_corr.tif"))
+tif <- raster(paste0(outdir,"/", shp.name,"/month/aug_2013-2020_corr.tif"))
 png(paste0(outdir,"/plots/aug.png"))
 spplot(tif,
        main=list(label="Aug mean precipitation (mm)",cex=1),
@@ -157,7 +157,7 @@ spplot(tif,
                         list(comarques, fill=NA, first=FALSE)))
 # Close the png file
 dev.off() 
-tif <- raster(paste0(outdir,"/", shp.name,"/month/sep_2013-2019_corr.tif"))
+tif <- raster(paste0(outdir,"/", shp.name,"/month/sep_2013-2020_corr.tif"))
 png(paste0(outdir,"/plots/sep.png"))
 spplot(tif,
        main=list(label="Sep mean precipitation (mm)",cex=1),
@@ -170,7 +170,7 @@ spplot(tif,
                         list(comarques, fill=NA, first=FALSE)))
 # Close the png file
 dev.off() 
-tif <- raster(paste0(outdir,"/", shp.name,"/month/oct_2013-2018_corr.tif"))
+tif <- raster(paste0(outdir,"/", shp.name,"/month/oct_2013-2020_corr.tif"))
 png(paste0(outdir,"/plots/oct.png"))
 spplot(tif,
        main=list(label="Oct mean precipitation (mm)",cex=1),
@@ -183,7 +183,7 @@ spplot(tif,
                         list(comarques, fill=NA, first=FALSE)))
 # Close the png file
 dev.off() 
-tif <- raster(paste0(outdir,"/", shp.name,"/month/nov_2013-2018_corr.tif"))
+tif <- raster(paste0(outdir,"/", shp.name,"/month/nov_2013-2020_corr.tif"))
 png(paste0(outdir,"/plots/nov.png"))
 spplot(tif,
        main=list(label="Nov mean precipitation (mm)",cex=1),
@@ -196,7 +196,7 @@ spplot(tif,
                         list(comarques, fill=NA, first=FALSE)))
 # Close the png file
 dev.off() 
-tif <- raster(paste0(outdir,"/", shp.name,"/month/dec_2013-2018_corr.tif"))
+tif <- raster(paste0(outdir,"/", shp.name,"/month/dec_2013-2020_corr.tif"))
 png(paste0(outdir,"plots/dec.png"))
 spplot(tif,
        main=list(label="Dec mean precipitation (mm)",cex=1),
