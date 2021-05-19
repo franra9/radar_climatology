fa07a6a35d35b9211852d2dac6bfaa107b1e6dbd
franra9 <francescrouradserias@gmail.com>
2021-05-19 13:30:00 +0200

    add normalization to ppt intensity histos, andadd map clim from intensity

--- R/histo.R
+++ R/histo.R
@@ -14,6 +14,13 @@ months <- c("jan",
             "oct",
             "nov",
             "dec")
+# this applies to hourly analysis normalize (do average) #
+if(any(months[2:3] == month)){
+  nor <- 2021 - 2013 +1
+} else {
+  nor <- 2020 - 2013 +1
+}
+###################################
 
 #for(month in months){
 #  event_high <- raster(paste0(outdir, shp.name, "/month_int/event_high_",month,".tif"))
@@ -57,13 +64,30 @@ months <- c("jan",
 
   shape <- shapefile("/home/francesc/data/radar_SMC_ppt_TFG/shape/LIASE_a_est_2.shp")
 
+  sum <- ppt_low + ppt_low*0
+  # maps from hourly basis
   # accumulation
-  sum <- ppt_high + ppt_mod + ppt_low
+  #sum <- ppt_high + ppt_mod + ppt_low
+  sum <- sum/nor
+  # plot raster also:
+  #tif <- sum
+  #png(paste0(outdir,"plots/hourly_clim_",month,".png"))
+  #spplot(tif,
+  #       main=list(label=paste0(month," mean precipitation (mm), hourly basis"),cex=1),
+  #       at=c(seq(minValue(tif), quantile(tif, 0.99), (quantile(tif, 0.99) - minValue(tif))/10),quantile(tif, 0.99) + 5 ),
+  #       col.regions = pal(400),
+  #       scales = list(draw = TRUE),
+  #       xlab = "longitude", ylab = "latitude",
+  #       sp.layout = list(list(area_a, fill=NA, first=FALSE), 
+  #                        list(east, fill=NA, first=FALSE),
+  #                        list(comarques, fill=NA, first=FALSE)))
+  ## Close the png file
+  #dev.off() 
+  
+  # histos  given raster and shapefile to split raster in 2 regions####
+  r_masked0 <- raster::mask(sum, shape,inverse=TRUE) #irr, west
+  r_masked1 <- raster::mask(sum, shape,inverse=FALSE) #non irr, east
 
-  r_masked0 <- raster::mask(sum, shape,inverse=TRUE) #irr
-  r_masked1 <- raster::mask(sum, shape,inverse=FALSE) #non irr
-
-  # histos
   a1 <- as.data.frame(r_masked1)
   a0 <- as.data.frame(r_masked0)
 
@@ -71,9 +95,9 @@ months <- c("jan",
   dat2 = data.frame(acc_ppt=Vectorize(a0$layer), group="west")
   dat = rbind(dat1, dat2)
   
-  png(paste0(outdir,"plots/hist_ppt_",month,".png"))
+  png(paste0(outdir,"plots/hist_lo_ppt_",month,".png"))
   ggplot(dat, aes(acc_ppt, fill=group, colour=group)) +
     geom_histogram(aes(y=..density..), alpha=0.6, 
                    position="identity", lwd=0.2) +
-    ggtitle("Normalized")
+    ggtitle(paste0("Normalized ppt distribution lo.int. ",month ))
   dev.off()
